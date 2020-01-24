e281c1b7c30879c427c0842b9e3349a67a9c21c1
Tom <tomtarpeydev@gmail.com>
2020-01-24 12:21:52 +0000

    first commit

---
+++ testing/chroot.sh
@@ -0,0 +1,24 @@
+#Potential variables: timezone, lang and local
+
+passwd
+
+TZuser=$(cat tzfinal.tmp)
+
+ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
+
+hwclock --systohc
+
+echo "LANG=en_GB.UTF-8" >> /etc/locale.conf
+echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_GB ISO-8859-1" >> /etc/locale.gen
+locale-gen
+
+pacman --noconfirm --needed -S networkmanager
+systemctl enable NetworkManager
+systemctl start NetworkManager
+pacman --noconfirm --needed -S linux
+pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
+
+pacman --noconfirm --needed -S dialog
+tarbs() { curl -O https://raw.githubusercontent.com/DecaLinuxFiles/TARBS/master/tarbs.sh && bash tarbs.sh ;}
+dialog --title "Install Tom's Rice" --yesno "This install script will easily let you access Tom's Auto-Rice Boostrapping Scripts (TARBS) which automatically install a full Arch Linux i3-gaps desktop environment.\n\nIf you'd like to install this, select yes, otherwise select no.\n\nLuke"  15 60 && tarbs
